e8fb3e1c90fea2b99e802ee3f130dd40daa48912
SteiKnight <delangono@gmail.com>
2023-06-23 10:27:59 +0200

    Completed Animation for side maps and the nav bar

--- src/HeroText.tsx
+++ src/HeroText.tsx
@@ -16,16 +16,19 @@ const HeroText = () => {
         initial="hiddenVariant"
         animate="revealedVariant"
         transition={{ delay: animationStart + 1, duration: 0.5 }}
-        className="flex flex-col text-3vw <md:text-24px font-bold mb-30px pt-100px"
+        className="flex flex-col text-3vw <md:text-24px font-bold mb-30px"
       >
         <span className="flex items-center gap-10px">
           Get There <Compass /> Get Back
         </span>
       </motion.div>
-      <motion.span variants={reveal}
+      <motion.span
+        variants={reveal}
         initial="hiddenVariant"
         animate="revealedVariant"
-        transition={{ delay: animationStart + 1.2, duration: 0.5 }} className="-30px w-1/4 <lg:w-1/3 <md:w-1/2 text-14px leading-tight">
+        transition={{ delay: animationStart + 1.2, duration: 0.5 }}
+        className="mb-30px w-1/4 <lg:w-1/3 <md:w-1/2 text-14px leading-tight"
+      >
         tech-driven, affordable, and convenient ride sharing. Cutting-edge
         technology, low fares, and seamless booking. Your everyday hero for
         hassle-free commuting.
--- src/MapImages.tsx
+++ src/MapImages.tsx
@@ -1,7 +1,25 @@
+import { motion } from "framer-motion";
+import { animationStart } from "./utils/animation";
+
 const MapImages = () => {
    return <div className="absolute w-full flex justify-between transform -translate-y-200px -z-1 overflow-hidden">
-      <img src="src/images/map_1.png" />
-      <img src="src/images/map_2.png" />
+      <motion.img src="src/images/map_1.png"
+         initial={{ x: -100, opacity: 0 }}
+         animate={{ x: 0, opacity: 1 }}
+         transition={{
+            delay: animationStart + 1.2,
+            type: "tween",
+            duration: 0.5,
+         }} className="object-cover"
+      />
+      <motion.img src="src/images/map_2.png"
+      initial={{ x: 100, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+         delay: animationStart + 1.2,
+         type: "tween",
+         duration: 0.5,
+      }} className="object-cover"/>
   </div>;
 };
 
