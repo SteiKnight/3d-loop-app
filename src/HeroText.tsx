9cbc3af4c9e158080f1e8d6ccfcf549364c8f66b
SteiKnight <delangono@gmail.com>
2023-06-23 07:59:45 +0200

    Added code for Comapss

--- src/HeroText.tsx
+++ src/HeroText.tsx
@@ -1,8 +1,19 @@
+import Compass from "./Compass";
 
 const HeroText = () => {
   return (
-    <div>HeroText</div>
-  )
-}
+    <div className="flex flex-col items-center text-center pt-100px">
+      <div className="flex flex-col text-3vw <md:text-24px font-bold mb-30px">
+           <span className="flex items-center gap-10px">Get There <Compass/> Get Back</span>
 
-export default HeroText
+      </div>
+      <span className="-30px w-1/4 <lg:w-1/3 <md:w-1/2 text-14px leading-tight">
+        tech-driven, affordable, and convenient ride sharing. Cutting-edge
+        technology, low fares, and seamless booking. Your everyday hero for
+        hassle-free commuting.
+      </span>
+    </div>
+  );
+};
+
+export default HeroText;
