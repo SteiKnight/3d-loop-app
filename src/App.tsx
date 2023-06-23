5f708d49fd58e897ac99267483ed06b4387be85f
SteiKnight <delangono@gmail.com>
2023-06-23 06:22:55 +0200

    add: Home and Navbar Elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
 
   return (
     <>
-  Hello!
+      <Navbar />
+      <Home/>
     </>
   )
 }
---
+++ src/Navbar.tsx
@@ -0,0 +1,11 @@
+import Logo from './Logo';
+
+const Navbar = () => {
+  return (
+    <div className="w-full flex items-center justify-around h-80px top-0 bg-white z-100">
+      <Logo/>
+    </div>
+  );
+};
+
+export default Navbar;
