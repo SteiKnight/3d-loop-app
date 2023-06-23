52a05410bc1a5808725a9daa076e7cf7742f08d0
SteiKnight <delangono@gmail.com>
2023-06-23 08:34:00 +0200

    updated: MapImages.tsx

--- src/MapImages.tsx
+++ src/MapImages.tsx
@@ -1,8 +1,8 @@
-
 const MapImages = () => {
-  return (
-    <div>MapImages</div>
-  )
-}
+   return <div className="absolute w-full flex justify-between transform -translate-y-200px -z-1 overflow-hidden">
+      <img src="src/images/map_1.png" />
+      <img src="src/images/map_2.png" />
+  </div>;
+};
 
-export default MapImages
+export default MapImages;
