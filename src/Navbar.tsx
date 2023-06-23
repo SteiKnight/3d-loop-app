ad136bfa6df753751ed2382bcc72db8e41fb64af
SteiKnight <delangono@gmail.com>
2023-06-23 07:11:06 +0200

    continuing on new branch with an arrow icon and an edited navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,9 +1,18 @@
-import Logo from './Logo';
+import ArrowRight from "./ArrowRight";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <div className="w-full flex items-center justify-around h-80px top-0 bg-white z-100">
-      <Logo/>
+      <Logo />
+      <div className="flex gap-20px items-center <md:hidden">
+        <span className="cursor-pointer">Affiliates</span>
+        <span className="cursor-pointer">Contact us</span>
+        </div>
+        <div className="flex gap-5px items-center cursor-pointer">
+           <span className="mb-3px">start loop</span>
+           <ArrowRight/>
+        </div>
     </div>
   );
 };
