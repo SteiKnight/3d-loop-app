60c39f91b8371095296d47cb70f473dcefc8271e
SteiKnight <delangono@gmail.com>
2023-06-23 06:24:43 +0200

    add: all other packages => also the current state has no errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,6 @@ export default defineConfig({
           ],
         ],
       },
-    }),
+    }), WindiCSS()
   ]
 })
